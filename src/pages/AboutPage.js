33187b1b3a4afc6d712f6e797753d4108b80448c
Ryan <ryanloweth29@gmail.com>
2021-03-24 20:10:32 -0700

    finished the basic front-end of the contact page with minimal styling

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import Jumbo from '../components/Jumbo';
+import Content from '../components/Content';
 
 function AboutPage(props) {
 
     return(
-        <Jumbo title={props.title} subTitle={props.subTitle} text = {props.text} />
+        <div>
+            <Jumbo title={props.title} />
+
+            <Content>
+            <p> Hello </p>
+            </Content>
+        </div>
     );
 
 }
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,12 +1,76 @@
 import React from 'react';
 
 import Jumbo from '../components/Jumbo';
+import Content from '../components/Content';
+import { Form , Button} from 'react-bootstrap';
 
-function ContactPage(props) {
+class ContactPage extends React.Component {
 
-    return(
-        <Jumbo title={props.title} subTitle={props.subTitle} text = {props.text} />
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            disabled: false,
+            emailSent: null
+        }
+    }
+
+    handleChange = (event) => {
+
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            disabled: true
+        })
+    }
+
+    render() {
+        
+        return(
+            <div>
+                <Jumbo title={this.props.title} />
+
+                <Content>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group>
+                            <Form.Label htmlFor="full-name">Full Name</Form.Label>
+                            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label htmlFor="email">Email</Form.Label>
+                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label htmlFor="message">Message</Form.Label>
+                            <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled} >Submit</Button>
+
+
+                        {this.state.emailSent === true && <p className="d-inline success-msg" >Email dent!</p>}
+                        {this.state.emailSent === false && <p className="d-inline err-msg" >Something went wrong, your email was not sent!</p>}
+                    </Form>
+                </Content>
+            </div>
+        );
+
+
+    }
 
 }
 
